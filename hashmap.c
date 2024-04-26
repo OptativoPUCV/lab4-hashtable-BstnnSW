a24f2ac0fb102918e3343830157979fd0dfc1608
Bstnn <106119476+BstnnSW@users.noreply.github.com>
2024-04-26 13:49:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -137,7 +137,7 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
   
   int pos = (map->current + 1) % map->capacity;
-  
+  printf("%d\n", pos);
 
   while (pos != map->current) {
     if (map->buckets[pos] != NULL && map->buckets[pos]->key != NULL) {
@@ -146,5 +146,6 @@ Pair * nextMap(HashMap * map) {
     }
     pos = (pos + 1) % map->capacity;
   }
+  
   return NULL; 
 }
